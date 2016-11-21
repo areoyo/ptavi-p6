034c0f4f3b6498431fa063d38272575a697dd1ed
Alejandra Delgado Reoyo <areoyo@gamma28.aulas.gsyc.urjc.es>
2016-11-21 11:02:55 +0100

    Programa cliente sin errores

--- client.py
+++ client.py
@@ -4,27 +4,38 @@
 Programa cliente que abre un socket a un servidor
 """
 
+import sys
 import socket
 
-# Cliente UDP simple.
+if len(sys.argv) != 3:
+   sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+METODO = sys.argv[1]
+LOGIN = sys.argv[2].split('@')[0]
+SERVER = sys.argv[2].split('@')[1].split(':')[0]
+PORT = int(sys.argv[2].split(':')[-1])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
+LINE = METODO + " sip:" + LOGIN + "@" + SERVER + " SIP/2.0\r\n\r\n"
+
+if METODO == 'INVITE' or 'ACK' or 'BYE':
+    print("Enviando: " + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
+
+datos = data.decode('utf-8').split()
+if datos[2] == 'Trying' and datos[8] == 'OK':
+    METODO = 'ACK'
+    LINE = METODO + " sip:" + LOGIN + "@" + SERVER + " SIP/2.0\r\n\r\n"
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+
 print("Terminando socket...")
 
 # Cerramos todo
